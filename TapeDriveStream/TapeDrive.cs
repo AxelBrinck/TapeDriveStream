8dd5b12b512afbed8a3cdb45a196a7785e94abb1
Axel Brinck <axel.louis.1991@gmail.com>
2020-12-08 18:49:53 +0100

    comments added

--- TapeDriveStream/TapeDrive.cs
+++ TapeDriveStream/TapeDrive.cs
@@ -5,8 +5,25 @@ namespace TapeDriveStream
 {
     public abstract class TapeDrive : IDisposable
     {
+        /// <summary>
+        /// Access to the underlying stream provided when
+        /// <see cref="UnderlyingStream"/> was initialized.
+        /// </summary>
+        /// <para>
+        /// A change in the underlying stream could lead to data-loss or even
+        /// data-corruption.
+        /// </para>
+        /// <value>The underlying stream.</value>
         public Stream UnderlyingStream { get; }
 
+        /// <summary>
+        /// Initializes a new instance of <see cref="UnderlyingStream"/> by
+        /// providing a stream.
+        /// </summary>
+        /// <exception cref="System.InvalidOperationException">
+        /// Thrown when unreadable/unwritable/unseekable stream is provided.
+        /// </exception>
+        /// <param name="stream"></param>
         public TapeDrive(Stream stream)
         {
             if (!stream.CanRead)
